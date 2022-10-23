e55e1d60582717f90710241968b8a28f41f7536d
bagusyanuar <bagus.yanuar613@gmail.com>
2022-10-23 16:58:49 +0700

    landing page

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -1,7 +1,159 @@
 @extends('layout')
 
 @section('content')
-<div class="hero">
+    <section id="hero">
+        <div class="pt-3 pr-5 pl-5">
+            <div class="hero p-5 align-items-center d-flex">
+                <div class="row flex-column-reverse flex-lg-row align-items-center">
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <h1 class="font-weight-bold " style="font-size: 44px">Layanan Pengaduan
+                            Online Masyarakat</h1>
+                        <p style="font-size: 20px;">Sampaikan laporan Anda langsung kepada instansi pemerintah
+                            berwenang</p>
+                        <div class="d-flex">
+                            <a href="#" class="main-button">Buat Pengaduan</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <img src="{{ asset('/assets/icons/hero.png') }}" class="w-100">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="about-us" class="mt-5">
+        <div class="pr-5 pl-5">
+            <h2 class="font-weight-bold text-center">Tentang Kami</h2>
+            <p style="font-size: 18px;" class="text-center mb-5">LPOM adalah Aplikasi Pengelolaan Pengaduan Masyarakat
+                dan pelayanan terhadap semua aspirasi dan pengaduan masyarakat.</p>
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <img src="{{ asset('/assets/icons/about-us-hero.png') }}" class="w-100">
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <p class="f18 text-justify mb-5">
+                        Pengelolaan pengaduan pelayanan publik disetiap organisasi penyelenggara di Indonesia belum
+                        terkelola secara efektif dan teritegrasi. Oleh karena itu LPOM hadir untuk mengatasi masalah
+                        tersebut. Dengan adanya LPOM masyarakat bisa melakukan pengaduan secara online.
+                    </p>
+                    <div class="d-flex mb-4">
+                        <div class="mr-3">
+                            <div class="circle-palette">
+                                <i class="mdi mdi-shopping-outline" style="font-size: 40px;"></i>
+                            </div>
+                        </div>
+                        <div class="flex-grow-1">
+                            <p class="font-weight-bold mb-1">Meningkatkan Mutu Pelayanan</p>
+                            <p class="text-justify">Dengan adanya Aplikasi LPOM ini diharapkan dapat meningkatkan
+                                pelayanan terhadap masyarakat.</p>
+                        </div>
+                    </div>
+                    <div class="d-flex mb-4">
+                        <div class="mr-3">
+                            <div class="circle-palette">
+                                <i class="mdi mdi-image-outline" style="font-size: 40px;"></i>
+                            </div>
+                        </div>
+                        <div class="flex-grow-1">
+                            <p class="font-weight-bold mb-1">Praktis Dan Hemat Waktu</p>
+                            <p class="text-justify">Dengan adanya LPOM ini masyarakat tidak perlu datang ke instansi
+                                terkait untuk melakukan pengaduan.</p>
+                        </div>
+                    </div>
+                    <div class="d-flex">
+                        <div class="mr-3">
+                            <div class="circle-palette">
+                                <i class="mdi mdi-chart-bar" style="font-size: 40px;"></i>
+                            </div>
+                        </div>
+                        <div class="flex-grow-1">
+                            <p class="font-weight-bold mb-1">Cepat, Tepat dan Tuntas</p>
+                            <p class="text-justify">Pengaduan dapat terkoordinasi dengan baik, sehingga dapat
+                                mempercepat proses tindak lanjut.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="procedure" class="mt-5">
+        <div class="pr-5 pl-5">
+            <h2 class="font-weight-bold text-center">Prosedur Pengaduan</h2>
+            <p style="font-size: 18px;" class="text-center mb-5">Berikut adalah prosedur pengaduan</p>
+            <div class="d-flex justify-content-center w-100 mb-5">
+                <div class="row w-75 justify-content-center">
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <div class="procedure-palette p-5 d-flex align-items-center">
+                            <div>
+                                <p class="font-weight-bold mb-0">1. Daftar / Masuk</p>
+                                <p class="text-justify">Daftar terlebih dahulu sebelum melakukan pengaduan. Setelah
+                                    mendaftar, login untuk menulis pengaduan anda.</p>
 
-</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <div class="procedure-palette p-5 d-flex align-items-center">
+                            <div>
+                                <p class="font-weight-bold mb-0">2. Tulis Pengaduan</p>
+                                <p class="text-justify">Tulis pengaduan yang sesuai dengan masalah yang anda alami atau
+                                    yang anda temui.Sertakan surat atau foto sebelum melakukan pengaduan.</p>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex justify-content-center w-100 mb-5">
+                <div class="row w-75 justify-content-center">
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <div class="procedure-palette p-5 d-flex align-items-center">
+                            <div>
+                                <p class="font-weight-bold mb-0">3. Verifikasi Pengaduan</p>
+                                <p class="text-justify">Jika anda sudah mengirim pengaduan, maka pengaduan anda akan
+                                    segera di proses dan diverifikasi oleh petugas kami.</p>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <div class="procedure-palette p-5 d-flex align-items-center">
+                            <div>
+                                <p class="font-weight-bold mb-0">4. Proses Tindak Lanjut</p>
+                                <p class="text-justify">Setelah melalui proses verifikasi oleh petugas, selanjutnya kami
+                                    akan menindak lanjuti terkait pengaduan anda.</p>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex justify-content-center w-100 mb-5">
+                <div class="row w-75 justify-content-center">
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <div class="procedure-palette p-5 d-flex align-items-center">
+                            <div>
+                                <p class="font-weight-bold mb-0">5. Beri Tanggapan</p>
+                                <p class="text-justify">Setelah melalui proses verifikasi oleh petugas, selanjutnya kami
+                                    akan menindak lanjuti terkait pengaduan anda.</p>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12">
+                        <div class="procedure-palette p-5 d-flex align-items-center">
+                            <div>
+                                <p class="font-weight-bold mb-0">6. Selesai</p>
+                                <p class="text-justify">Anda bisa melihat tanggapan atau status dari pengaduan anda di
+                                    halaman dashboard anda.</p>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </section>
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,46 +8,41 @@
     <meta name="csrf-token" content="{{csrf_token()}}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link
-        href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
-        rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/@mdi/font@6.9.96/css/materialdesignicons.min.css" rel="stylesheet">
     <link href="{{ asset('/css/style.css') }}" rel="stylesheet">
     <title>Pengaduan</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top custom-nav">
+<nav class="navbar navbar-expand-lg navbar-light bg-light custom-nav">
     <a class="navbar-brand" href="#">
         <div class="d-flex align-items-center">
-            <img src="{{ asset('assets/icons/customer-service.png') }}" height="50" class="mr-2">
-            <span class="f14" style="color: whitesmoke">PENGADUAN</span>
+            <img src="{{ asset('assets/icons/headset.png') }}" height="50" class="mr-2">
+            <span class="f14" style="color: var(--primaryColor); font-weight: bold; letter-spacing: 1.5px">PENGADUAN</span>
         </div>
-
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
+        <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="/">Beranda <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
+                <a class="nav-link" href="#">Tentang</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
+                <a class="nav-link" href="#">Statistik</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Prosedur</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Kontak</a>
             </li>
         </ul>
     </div>
@@ -61,6 +56,5 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
         crossorigin="anonymous"></script>
-<a href="https://www.flaticon.com/free-icons/customer-support" title="customer support icons">Customer support icons created by sonnycandra - Flaticon</a>
 </body>
 </html>
