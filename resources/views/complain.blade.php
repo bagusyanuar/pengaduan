73d548cc17b1143474c6a067e5b1768d52d4dcfa
bagusyanuar <bagus.yanuar613@gmail.com>
2022-10-25 12:56:18 +0700

    Update complain.blade.php

--- resources/views/complain.blade.php
+++ resources/views/complain.blade.php
@@ -11,34 +11,75 @@
                     <div class="d-flex justify-content-center w-100">
                         <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
                             <li class="nav-item">
-                                <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Personal</a>
+                                <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home"
+                                   role="tab" aria-controls="pills-home" aria-selected="true">Personal</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Badan Hukum / Organisasi</a>
+                                <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile"
+                                   role="tab" aria-controls="pills-profile" aria-selected="false">Badan Hukum /
+                                    Organisasi</a>
                             </li>
                         </ul>
                     </div>
                     <div class="tab-content" id="pills-tabContent">
-                        <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+                        <div class="tab-pane fade show active" id="pills-home" role="tabpanel"
+                             aria-labelledby="pills-home-tab">
                             <div class="card">
                                 <div class="card-body">
                                     <form>
                                         @csrf
-                                        <div class="form-group">
+                                        <div class="form-group mb-1">
                                             <label for="name">Nama</label>
-                                            <input type="text" class="form-control" id="name" aria-describedby="emailHelp" placeholder="Nama">
-                                            <small id="nameHelp" class="form-text text-muted">Contoh : Yanuar Ihsan</small>
+                                            <input type="text" class="form-control" id="name"
+                                                   aria-describedby="emailHelp" placeholder="Nama">
+                                            <small id="nameHelp" class="form-text text-muted">Contoh : Yanuar
+                                                Ihsan</small>
                                         </div>
-                                        <div class="form-group">
+                                        <div class="form-group mb-1">
                                             <label for="address">Alamat</label>
-                                            <textarea rows="3" class="form-control" id="address" aria-describedby="addressHelp" placeholder="Alamat"></textarea>
-                                            <small id="addressHelp" class="form-text text-muted">Contoh : Jl. Arjuna no. 16, Kec. Serengan, Surakarta</small>
+                                            <textarea rows="3" class="form-control" id="address"
+                                                      aria-describedby="addressHelp" placeholder="Alamat"></textarea>
+                                            <small id="addressHelp" class="form-text text-muted">Contoh : Jl. Arjuna no.
+                                                16, Kec. Serengan, Surakarta</small>
+                                        </div>
+                                        <div class="form-group mb-1">
+                                            <label for="job">Pekerjaan</label>
+                                            <input type="text" class="form-control" id="job" aria-describedby="jobHelp"
+                                                   placeholder="Pekerjaan">
+                                            <small id="jobHelp" class="form-text text-muted">Contoh : Wiraswasta</small>
+                                        </div>
+                                        <div class="form-group mb-1">
+                                            <label for="phone">No. Whatsapp</label>
+                                            <div class="input-group">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text" id="basic-addon1">+62</span>
+                                                </div>
+                                                <input type="number" class="form-control" id="phone"
+                                                       aria-describedby="phoneHelp" placeholder="No. Whatsapp">
+                                            </div>
+                                            <small id="phoneHelp" class="form-text text-muted">Contoh :
+                                                895422630233</small>
+                                        </div>
+                                        <div class="form-group mb-1">
+                                            <label for="email">Email</label>
+                                            <input type="email" class="form-control" id="job" aria-describedby="emailHelp"
+                                                   placeholder="Email">
+                                            <small id="emailHelp" class="form-text text-muted">Contoh : pelapor@gmail.com</small>
+                                        </div>
+                                        <div class="form-group mb-1">
+                                            <label for="complain">Saran / Pengaduan</label>
+                                            <textarea rows="3" class="form-control" id="complain"
+                                                      aria-describedby="complainHelp" placeholder="Saran / Pengaduan"></textarea>
+                                            <small id="complainHelp" class="form-text text-muted">Contoh : Jl. Arjuna no.
+                                                16, Kec. Serengan, Surakarta</small>
                                         </div>
                                     </form>
                                 </div>
                             </div>
                         </div>
-                        <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...</div>
+                        <div class="tab-pane fade" id="pills-profile" role="tabpanel"
+                             aria-labelledby="pills-profile-tab">...
+                        </div>
                     </div>
                 </div>
             </div>
